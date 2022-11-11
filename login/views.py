52f921c57e5a7677335c31b1ca7d94173cfacac6
Spencer Sorenson <59028483+Anotherspencer1@users.noreply.github.com>
2022-11-11 06:46:30 -0600

    Adding login and register views

---
+++ login/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def login(request):
+    return HttpResponse('<h1>Login To Your Account</h1>')
+
+def register(request):
+    return HttpResponse('<h1>Register New User</h1>')
